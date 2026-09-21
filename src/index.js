94a15f98fba8374548a06da483e5c3b3a4762601
hosseinasgari898989-dev <hosseinasgari898989@gmail.com>
2026-09-21 21:59:32 +0330

    fix: allow admin credential delete preflight

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const ALLOWED_ORIGINS = [
 
 const CORS = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Admin-Token',
 };
 
